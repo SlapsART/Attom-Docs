854fa1fe31a3dc3ba5828f4965ac4c45ea425dcd
Christian Aldana <davidsanta391@gmail.com>
2025-10-16 15:34:39 -0500

    Publish image and icons section

--- astro.config.mjs
+++ astro.config.mjs
@@ -36,7 +36,7 @@ export default defineConfig({
 					items: [
 						// Each item here is one entry in the navigation menu.
 						{ label: 'Acerca de Attom DS', slug: 'introduccion/acerca-de-attom' },
-						{ label: 'Actualizaciones', slug: 'introduccion/actualizaciones' },
+						//{ label: 'Actualizaciones', slug: 'introduccion/actualizaciones' },
 						{ label: 'Versionamiento', slug: 'introduccion/versiones' },
 					],
 				},
